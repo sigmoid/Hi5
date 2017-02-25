17b1389aa2e997018cde03c4758ab85c1ffcdec0
EAE Admin <EAE Admin>
2017-02-24 20:37:18 -0700

    Working on hand collisions in my own scene.

---
+++ Assets/HandCollision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandCollision : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("seeck hi5");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //print("collision: STOP IT");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //print("trigger: seeck hi5");
+
+        if (other.tag == "Palm")
+        {
+            print("SICK HYFYVE");
+            Destroy(other.transform.parent.gameObject);
+        }
+        else if (other.tag == "NotPalm")
+            print("THAT WAS NOT A HYFYVE");
+    }
+}

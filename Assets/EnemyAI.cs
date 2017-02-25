59f3af4b0e01a01be44e829d0f7d94e3b39d87e1
EAE Admin <EAE Admin>
2017-02-24 23:34:46 -0700

    Added a simple up and down AI script to the TestFist scene.

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    public bool UpDown;
+    public int upRange;
+    public int downRange;
+
+    private bool goingUp;
+
+	// Use this for initialization
+	void Start () {
+        if (UpDown)
+            goingUp = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (UpDown)
+        {
+            if (goingUp)
+            {
+                if (this.transform.position.y < upRange)
+                    goingUp = false;
+                else
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.01f, this.transform.position.z);
+            }
+            else
+            {
+                if (this.transform.position.y > downRange)
+                    goingUp = true;
+                else
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.01f, this.transform.position.z);
+            }
+        }
+	}
+}

819ca5a3965817e8d77c31e799263bfb4ae97a47
Adam Waggoner <adam.swaggoner@gmail.com>
2017-02-24 18:59:52 -0700

    Import Models

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmController : MonoBehaviour {
+
+
+    public Transform UpperArm, ForeArm, Hand;
+    private Transform _ShoulderAnchor, _ElbowAnchor, _WristAnchor;
+
+	// Use this for initialization
+	void Start () {
+        _ShoulderAnchor = GameObject.Find("ShoulderAnchor").transform;
+        _ElbowAnchor = GameObject.Find("ElbowAnchor").transform;
+        _WristAnchor = GameObject.Find("WristAnchor").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+
+	}
+}

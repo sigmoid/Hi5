54181fb239cc5e04d4628475d5f7f5f9438543ed
Adam Waggoner <adam.swaggoner@gmail.com>
2017-02-25 01:47:10 -0700

    Fix gamecontroller prefab

--- Assets/button_manager.cs
+++ Assets/button_manager.cs
@@ -3,6 +3,11 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 public class button_manager : MonoBehaviour
 {
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
     public void NewGameBtn(string newGameLevel)
     {
         SceneManager.LoadScene(newGameLevel);

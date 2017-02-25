36c49719050bb2c0ba76fb1660cda743c8b33c88
Christian Roy <christian.50.roy@gmail.com>
2017-02-24 22:35:34 -0700

    Improved Zoom
    Zoom has been improved

--- Assets/ZoomUI.cs
+++ Assets/ZoomUI.cs
@@ -5,26 +5,32 @@ using UnityEngine.UI;
 public class ZoomUI : MonoBehaviour {
 
     [SerializeField]
+    private int AnimationTime;
+    [SerializeField]
     private int maxScale;
+    [SerializeField]
+    private AnimationCurve curve;
 
     private RectTransform _image;
+    private float _animTime;
 
 	// Use this for initialization
 	void Start ()
     { 
         _image = GetComponent<RectTransform>();
         _image.localScale = new Vector3(0, 0, 1);
+        _animTime = 0;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        float x = _image.localScale.x;
-        float y = _image.localScale.y;
 
-        if (_image.localScale.x <= maxScale)
-            _image.localScale += new Vector3(.5f, .5f, 0);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        _animTime += Time.deltaTime;
+
+        float x = curve.Evaluate(_animTime / AnimationTime);
+
+        _image.localScale = new Vector3(maxScale * x, maxScale * x, 1);
+    }
 
 
 }
